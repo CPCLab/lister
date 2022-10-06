f4adab2360658e04e6a67b23e2753c1338e0a0e4
fathoni <fathoni.am@gmail.com>
2022-10-06 12:58:33 +0200

    fixes for layout mess on windows 11

--- main_gui.py
+++ main_gui.py
@@ -55,20 +55,21 @@ class InitialGUI(GeneralGUI):
 
     def create_widgets(self):
 
+        ttk.Style().configure('Font.TLabelframe', background="#ebebec")
+
         # REQUIRED ARGUMENT FRAME
-        self.req_label_frame = ttk.LabelFrame(master=self, text="Required arguments")
+        self.req_label_frame = ttk.LabelFrame(master=self, text="Required arguments", style="Font.TLabelframe")
         self.req_label_frame.grid(row=1, column=0, sticky="nswe", padx=10, pady=(30,10))
-        self.required_argument_frame = customtkinter.CTkFrame(master=self.req_label_frame)
+        self.required_argument_frame = customtkinter.CTkFrame(master=self.req_label_frame, width=100)
         self.required_argument_frame.grid_rowconfigure(1, weight=0)
         self.required_argument_frame.grid(row=1, column=0, sticky="nswe", padx=10, pady=10)
 
         # OUTPUT FILE NAME
         self.output_filename_label = customtkinter.CTkLabel(
-            master=self.required_argument_frame, text="Output File Name:", justify="right", anchor=customtkinter.W)
+            master=self.required_argument_frame, text="Output File Name:", justify="right", anchor=customtkinter.W, text_font=("", '10','bold'))
         self.output_filename_label.grid(column=0, row=0, sticky="w", pady=(20,0), padx=5)
         self.output_filename_info = customtkinter.CTkLabel(
-            master=self.required_argument_frame, text="Filename for your metadata and outputs, without extension.",
-            text_font=("",11))
+            master=self.required_argument_frame, text="Filename for your metadata and outputs, without extension.")
         self.output_filename_info.grid(column=0, row=1, sticky="w", pady=0, padx=5)
         self.elabftw_filename_entry = customtkinter.CTkEntry(
             master=self.required_argument_frame, width=300, placeholder_text="Output File Name", border_width=1)
@@ -76,11 +77,11 @@ class InitialGUI(GeneralGUI):
 
         # ELABFTW ID
         self.elabftw_id_label = customtkinter.CTkLabel(
-            master=self.required_argument_frame, text="eLabFTW Experiment ID:")
+            master=self.required_argument_frame, text="eLabFTW Experiment ID:", text_font=("", '10','bold'))
         self.elabftw_id_label.grid(column=1,row=0, pady=(20,0), padx=25, sticky="w", columnspan=4)
         self.elabftw_id_info = customtkinter.CTkLabel(
             master=self.required_argument_frame,
-            text="Integer indicated in the URL of the experiment.",  text_font=("",11))
+            text="Integer indicated in the URL of the experiment.")
         self.elabftw_id_info.grid(column=1,row=1, pady=0, padx=25, sticky="w", columnspan=4)
         self.elabftw_id_entry = customtkinter.CTkEntry(
             master=self.required_argument_frame, width=200, placeholder_text="eLabFTW ID", border_width=1)
@@ -88,12 +89,11 @@ class InitialGUI(GeneralGUI):
 
         # ELABFTW ENDPOINT
         self.elabftw_endpoint_label = customtkinter.CTkLabel(
-            master=self.required_argument_frame, text="eLabFTW API endpoint URL:")
+            master=self.required_argument_frame, text="eLabFTW API endpoint URL:", text_font=("", '10','bold'))
         self.elabftw_endpoint_label.grid(column=0, row=3, sticky="w", pady=(20,0), padx=5)
         self.elabftw_endpoint_info = customtkinter.CTkLabel(
             master=self.required_argument_frame,
-            text="This would look like [your eLabFTW URL]/api/v1, or please ask your administrator.",
-            text_font=("",11))
+            text="[Your eLabFTW URL]/api/v1 - please ask the admin.")
         self.elabftw_endpoint_info.grid(column=0, row=4, sticky="w", pady=0, padx=5)
         self.elabftw_endpoint_entry = customtkinter.CTkEntry(
             master=self.required_argument_frame, width=300, placeholder_text="eLabFTW API endpoint URL", border_width=1)
@@ -101,14 +101,14 @@ class InitialGUI(GeneralGUI):
 
         # BASE OUTPUT DIR
         self.base_output_label = customtkinter.CTkLabel(
-            master=self.required_argument_frame, text="Base Output Directory:")
+            master=self.required_argument_frame, text="Base Output Directory:", text_font=("", '10','bold'))
         self.base_output_label.grid(column=1,row=3, pady=(20,0), padx=25, sticky="w", columnspan=4)
         self.base_output_info = customtkinter.CTkLabel(
-            master=self.required_argument_frame, text="Local directory to save the outputs.", text_font=("",11))
+            master=self.required_argument_frame, text="Local directory to save the outputs.")
         self.base_output_info.grid(column=1,row=4, pady=0, padx=25, sticky="w", columnspan=4)
         self.base_output_entry = customtkinter.CTkEntry(
             master=self.required_argument_frame,
-            width=200, placeholder_text="Base output directory", border_width=1)
+            width=295, placeholder_text="Base output directory", border_width=1)
         self.base_output_entry.grid(column=1, row=5,  pady=0, padx=25, sticky="w")
         self.output_dir_browse_button = customtkinter.CTkButton(
             master=self.required_argument_frame, text="Browse...", command=self.button_event)
@@ -116,24 +116,23 @@ class InitialGUI(GeneralGUI):
 
         # ELABFTW TOKEN
         self.elabftw_token_label = customtkinter.CTkLabel(
-            master=self.required_argument_frame, text="eLabFTW API Token:", anchor=customtkinter.W)
+            master=self.required_argument_frame, text="eLabFTW API Token:", anchor=customtkinter.W, text_font=("", '10','bold'))
         self.elabftw_token_label.grid(column=0,row=6, pady=(20,0), padx=5, sticky="w", columnspan=4)
         self.elabftw_token_label = customtkinter.CTkLabel(
-            master=self.required_argument_frame, text="Please ask your admin to generate an eLabFTW API token for you.",
-            text_font=("",11))
+            master=self.required_argument_frame, text="Please ask your admin to generate an eLabFTW API token for you.")
         self.elabftw_token_label.grid(column=0,row=7, pady=0, padx=5, sticky="w", columnspan=4)
         self.elabftw_token_entry = customtkinter.CTkEntry(
             master=self.required_argument_frame, width=600, placeholder_text="eLabFTW API Token", border_width=1)
         self.elabftw_token_entry.grid(column=0, row=8,  pady=(0,20), padx=5, sticky="w", columnspan=4)
 
         # OPTIONAL ARGS FRAME
-        self.optional_label_frame = ttk.LabelFrame(master=self, text="Optional arguments")
+        self.optional_label_frame = ttk.LabelFrame(master=self, text="Optional arguments",style="Font.TLabelframe")
         self.optional_label_frame.grid(row=2, column=0, sticky="nswe", padx=10, pady=(20,10))
         self.optional_argument_frame = customtkinter.CTkFrame(master=self.optional_label_frame)
         self.optional_argument_frame.grid(row=0, column=0, sticky="nswe", padx=10, pady=10)
         self.upl_to_elab_switch = customtkinter.CTkSwitch(
             master=self.optional_argument_frame, text="Upload extracted output to the corresponding eLabFTW entry",
-            command=self.switch_event, variable=self.switch_var, onvalue="on", offvalue="off")
+            command=self.switch_event, variable=self.switch_var, onvalue="on", offvalue="off", bg_color="#ebebec")
         self.upl_to_elab_switch.grid(row=1, column=0)
 
         # FINISHING STEP
@@ -167,7 +166,7 @@ class InitialGUI(GeneralGUI):
                      'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et ' \
                      'accusam et justo duo dolores et ea rebum Stet clita kasd gubergren, no sea takimata sanctus ' \
                      'est Lorem ipsum dolor sit amet.'
-        self.output_txtbox = tkinter.Text(master=self, state="normal", width=122, height=37)
+        self.output_txtbox = tkinter.Text(master=self, state="normal", width=122, height=30)
         self.output_txtbox.insert(tkinter.END, self.mytext)
         self.output_txtbox.configure(state='disabled')
         self.output_txtbox.grid(row=1, column=0, sticky="w", padx=25, pady=10)
